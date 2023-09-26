237a3056813457fd2cd4219512afb1999b41f418
tadams95 <tyrelleadams28@gmail.com>
2023-09-26 12:43:34 -0700

    intro to inheritance

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -1,7 +1,9 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract Faucet {
+import "./Owned.sol";
+
+contract Faucet is Owned {
     //this is a special function
     //it's called when you make a tx that doesn't specify
     //function name to call
@@ -12,16 +14,45 @@ contract Faucet {
     mapping(address => bool) private funders;
     mapping(uint => address) private lutFunders;
 
+    
+
+    modifier limitWithdraw(uint withdrawAmount) {
+        require(
+            withdrawAmount <= 100000000000000000,
+            "Cannot withdraw more than 0.1 ETH"
+        );
+        _;
+    }
+
+   
     receive() external payable {}
+
+    function transferOwnership(address newOwner) external onlyOwner {
+        owner = newOwner;
+    }
 
     function addFunds() external payable {
         address funder = msg.sender;
 
-        if (!funders[funder]){
+        if (!funders[funder]) {
             uint index = numOfFunders++;
             funders[funder] = true;
             lutFunders[index] = funder;
         }
+    }
+
+    function test1() external onlyOwner {
+        //some managing stuff that only admin should have access to
+    }
+
+    function test2() external onlyOwner {
+        //some managing stuff that only admin should have access to
+    }
+
+    function withdraw(
+        uint withdrawAmount
+    ) external limitWithdraw(withdrawAmount) {
+        payable(msg.sender).transfer(withdrawAmount);
     }
 
     function getAllFunders() public view returns (address[] memory) {
@@ -49,4 +80,12 @@ contract Faucet {
     //Nonce - a hash that when combined with the minHash proves that
     //the block has gone through proof of work(POW)
     // 8 BYTES => 64 BITS
+
+    //const instance = await Faucet.deployed();
+    //instance.addFunds({from: accounts[0], value: "2000000000000000000"})
+    //instance.addFunds({from: accounts[1], value: "2000000000000000000"})
+
+    //instance.withdraw("500000000000000000", {from: accounts[1]})
+    //instance.getFunderAtIndex(0)
+    //instance.getAllFunders()
 }
